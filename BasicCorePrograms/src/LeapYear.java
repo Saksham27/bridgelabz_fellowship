93688ac6e5eb7ce3932edc7c39b4579c7c019ba7
Saksham27 <geekysaksham27@gmail.com>
2020-03-03 15:48:06 +0530

    leap year program

---
+++ BasicCorePrograms/src/LeapYear.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LeapYear {
+	
+	public static boolean checkLeapYear(int y){
+		if( y%4 == 0 && y%100 != 0) {
+			return true;
+		}else if(y%400==0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	public static void main(String[] args) {
+		int year;
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+		
+		if( year < 1000 || year > 9999 ) {
+			System.out.println("Enter a valid year.");
+		}else {
+			if(checkLeapYear(year)) {
+				System.out.println(year+ " is a Leap year.");
+			}else {
+				System.out.println("Not a Leap year.");
+			}
+		}
+	}
+
+}
