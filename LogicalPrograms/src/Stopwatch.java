230a0a37a8b791b403c14056aa5531c8e7c11cb5
Saksham27 <geekysaksham27@gmail.com>
2020-03-04 11:18:57 +0530

    stopwatch program

--- LogicalPrograms/src/Stopwatch.java
+++ LogicalPrograms/src/Stopwatch.java
@@ -1,14 +1,45 @@
 import java.util.Scanner;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.text.DecimalFormat;
 
 public class Stopwatch {
-
+	
+	private static DecimalFormat df2 = new DecimalFormat("##");
+	
+	public static LocalTime startStopwatch() {
+		return java.time.LocalTime.now();
+	}
+	
+	public static LocalTime stopStopwatch() {
+		return java.time.LocalTime.now();
+	}
+	
+	public static String timeElapsed(LocalTime t1,LocalTime t2) {
+		String hours = df2.format(Duration.between(t2,t1).toHours());
+		String minutes = df2.format(Duration.between(t2,t1).toMinutes());
+		String seconds = df2.format(Duration.between(t2,t1).toSeconds());
+		return hours+" H:"+minutes+" M:"+seconds+" S";
+	}
 	public static void main(String[] args) {
-		char y;
-		char n;
+		
+		char c;
+		LocalTime t1=null,t2=null;
+		System.out.println("Press y when you want to start the stopwatch : ");
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("To Start the stopwatch press y : ")
-		y = sc.next
+		c = sc.next().charAt(0);
+		
+		if(c=='y' || c=='Y') {
+			t1=startStopwatch();
+		}
+		System.out.println("Now Press y when you want to stop the stopwatch : ");
+		c=sc.next().charAt(0);
+		if(c=='n' || c=='N') {
+			t2=stopStopwatch();
+		}
+		System.out.println(timeElapsed(t2,t1));
+		
+		
 	}
 
 }
