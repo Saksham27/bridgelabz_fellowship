7d11f06b9ca4a48ab5e677a34be7118d82813f27
Saksham27 <geekysaksham27@gmail.com>
2020-03-04 18:02:10 +0530

    sqrt function

--- JUnitTesting/src/MyUnitTest.java
+++ JUnitTesting/src/MyUnitTest.java
@@ -1,5 +1,6 @@
-import org.junit.Test;
+
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 	public class MyUnitTest{
 		
@@ -9,9 +10,25 @@ import static org.junit.Assert.*;
 			assertEquals("7",VendingMachine.change());
 		}
 		
-	
-	
-
+		@Test
+		public void testDayOfWeek() {
+			assertEquals("Wednesday",Util.dayOfWeek(4, 3, 2020));
+		}
+		
+		@Test
+		public void testTemperaturConversion() {
+			assertEquals(0,Util.temperaturConversion(32),0);
+		}
+		
+//		@Test
+//		public void testMonthlyPayment() {
+//			assertEquals(0,Util.monthlyPayment(1000,1,1),0);
+//		}
+		
+		@Test
+		public void testSqrt() {
+			assertEquals(2,Util.sqrt(4),0);
+		}
 
 	public static void main(String[] args) {
 		
---
+++ JUnitTesting/src/Util.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Util {
+	
+	
+	public static String dayOfWeek(int date,int month,int year) {
+		int y = year - (14-month)/12;
+		int x = y + y/4 - y/100 + y/400;
+		int m = month + 12*((14-month)/12)-2;
+		int d = (date + x + 31*m/12)%7;
+		switch(d) {
+		case 0:
+			return "Sunday";
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
+		default:
+			return "INVALID";
+		}
+	}
+	
+	public static double temperaturConversion(int fahrenheit) {
+		return (fahrenheit -32)*(5/9);
+	}
+	
+	public static double monthlyPayment(int principle,int year,double rate) {
+		int n = 12*year;
+		double r = rate/(12*100);
+		return (principle*r)/(1-Math.pow(1+r, -n));
+	}
+	
+	public static double sqrt(int n) {
+		double t=n;
+		double EPSILON = 1e-15;
+		while(Math.abs(t-n/t)>EPSILON*t) {
+			t=(n/t+t)/2;
+		}
+		return t;
+	}
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int date;
+		int month;
+		int year;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Date : ");
+		date=sc.nextInt();
+		System.out.print("Enter Month : ");
+		month=sc.nextInt();
+		System.out.print("Enter Date : ");
+		year=sc.nextInt();
+		if(month>=1 && month <=12 && date>=1 && date<=31 ) {
+			System.out.println("Day : "+dayOfWeek(date,month,year));
+		}
+	}
+
+}
