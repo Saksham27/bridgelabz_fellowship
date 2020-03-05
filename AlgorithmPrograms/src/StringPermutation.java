3b00e786a806daf26f04873e9d141494e5c0bd09
Saksham27 <geekysaksham27@gmail.com>
2020-03-05 13:01:52 +0530

    string permutation

--- AlgorithmPrograms/src/StringPermutation.java
+++ AlgorithmPrograms/src/StringPermutation.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
 public class StringPermutation {
-
+	
+	public static String swap(String s,int a, int b) {
+		char temp;
+		char[] arr = s.toCharArray();
+		temp  = arr[a];
+		arr[a]=arr[b];
+		arr[b]=temp;
+		return String.valueOf(arr);
+	}
+	
+	public static void stringPermutation(String s,int l,int e) {
+		
+		if(l==e) {
+			System.out.println(s);
+		}else {
+			for(int i=l;i<e;i++) {
+				s = swap(s,l,i);
+				stringPermutation(s,l+1,e);
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int length = s.length();
+		stringPermutation(s,0,length);
+		
 	}
 
 }
